56407b5892f6107c1ae645353359196e24b49a51
kapildavey <99812570+kapildavey@users.noreply.github.com>
2022-04-29 08:49:12 +0530

    Delete Core Java Assignments/Problem Statement 7- File –I+O directory

--- Core Java Assignments/Problem Statement 7- File –I+O/src/com/problem statement 7.2/NameNotValidException.java
+++
@@ -1,9 +0,0 @@
-package com.streambasedprogram;
-
-public class NameNotValidException extends Exception
-{
-    public String validname()
-    {
-         return ("Name is not Valid..Please ReEnter the Name");
-    }
-}
